bddc7279ed845c61d51f8bb755ee3e9d6ae18693
Arzul Paul <paul.arzul@he-arc.ch>
2019-04-12 08:50:51 +0200

    artworkController uses services

--- Look&Book/src/main/java/ch/hearc/dev/controller/ArtworkController.java
+++ Look&Book/src/main/java/ch/hearc/dev/controller/ArtworkController.java
@@ -7,19 +7,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import ch.hearc.dev.entity.Artwork;
 import repository.ArtworkRepository;
+import services.ArtworkService;
 
 
 @Controller
 public class ArtworkController {
 	ArtworkRepository artworkRepository;
 	
+	@Autowired
+	private ArtworkService userService;
+	
 	@GetMapping("/artworks")
-    public String getAll(Map<String, Object> model) {
-		
-		model.put("artworks", artworkRepository.findAll());
-		
-        return "artwork";
+    public Iterable<Artwork> getAll(Map<String, Object> model) {
+        return userService.findAll();
 	}
 	
 	/*@GetMapping("/artworks/{id}")
