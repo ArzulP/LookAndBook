cb1dec0237866de22145d75bc3a66d55b9054caa
Arzul Paul <paul.arzul@he-arc.ch>
2019-04-12 09:04:30 +0200

    changed folders

--- Look&Book/src/main/java/ch/hearc/dev/controller/ArtworkController.java
+++ Look&Book/src/main/java/ch/hearc/dev/controller/ArtworkController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import ch.hearc.dev.entity.Artwork;
-import repository.ArtworkRepository;
-import services.ArtworkService;
+import ch.hearc.dev.repository.ArtworkRepository;
+import ch.hearc.dev.services.ArtworkService;
 
 
 @Controller
@@ -17,11 +17,11 @@ public class ArtworkController {
 	ArtworkRepository artworkRepository;
 	
 	@Autowired
-	private ArtworkService userService;
+	private ArtworkService artworkService;
 	
 	@GetMapping("/artworks")
     public Iterable<Artwork> getAll(Map<String, Object> model) {
-        return userService.findAll();
+        return artworkService.findAll();
 	}
 	
 	/*@GetMapping("/artworks/{id}")
