a4c54ddf6fb1248d3440fb8ce6d43e8c71a3a29d
Dubois Jérémy <jeremy.dubois@he-arc.ch>
2019-04-29 16:15:21 +0200

    first working login and start html pages

--- Look&Book/src/main/java/ch/hearc/dev/config/WebSecurityConfiguration.java
+++ Look&Book/src/main/java/ch/hearc/dev/config/WebSecurityConfiguration.java
@@ -48,11 +48,19 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 		// The pages does not require login
 	    http.authorizeRequests().antMatchers("/", "/login", "/logout").permitAll();
 	     
+	    // /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
+        // If no login, it will redirect to /login page.
+        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+ 
+        // For ADMIN only.
+        http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
+        
 	    // When the user has logged in as XX.
         // But access a page that requires role YY,
         // AccessDeniedException will be thrown.
         http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
-	    // Config for Login Form
+	    
+        // Config for Login Form
 	    http.authorizeRequests().and().formLogin()//
 			// Submit URL of login page.
 			.loginProcessingUrl("/j_spring_security_check") // Submit URL
