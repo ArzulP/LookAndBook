6807bec888d8ede5ed740f179ae70181048cbcb8
Arzul Paul <paul.arzul@he-arc.ch>
2019-05-02 18:07:00 +0200

    modified katalon

---
+++ LookAndBookTest/Test Suites/Test.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a93f4f8a-5292-4528-888c-33a2393e4873</testSuiteGuid>
+</TestSuiteEntity>
